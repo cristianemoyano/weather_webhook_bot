850f202beddaac097834d5dc76eb982b2ca09b65
eb-cmoyano <cmoyano@eventbrite.com>
2018-09-05 18:15:59 -0300

    change iframe

--- views/main.py
+++ views/main.py
@@ -6,13 +6,13 @@ class IframeView(View):
         super(IframeView, self).__init__()
 
     def render(self):
-        msg = '<p>Try: Weather forecast in Mendoza today</p>'
+        msg = '<p>Try: find me 2 tickets to Madonnas concert in New York</p>'
         iframe = (
             '<iframe '
             'allow="microphone;"'
             'width="350"  '
             'height="430" '
-            'src="https://console.dialogflow.com/api-client/demo/embedded/1a033c63-e5f9-4f42-b715-1db32e561b52">'
+            'src="https://console.dialogflow.com/api-client/demo/embedded/59b4f5f8-6110-45dd-b110-82e42bc3bb47">'
             '</iframe> '
         )
         return msg + iframe

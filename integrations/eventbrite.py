525357da5db8f8b28ca99cca2a78d9d52ae9e0e6
eb-cmoyano <cmoyano@eventbrite.com>
2018-09-06 00:20:10 -0300

    add params to request in eb

---
+++ integrations/eventbrite.py
@@ -0,0 +1,23 @@
+from integrations.base import Integration
+from constants import EB_ACCESS_TOKEN
+from eventbrite import Eventbrite
+import urllib
+
+
+class EventbriteIntegration(Integration):
+    """docstring for EventbriteIntegration"""
+    def __init__(self):
+        super(EventbriteIntegration, self).__init__()
+        self.eb_token = EB_ACCESS_TOKEN
+
+    def respond(self, endpoint, target, params={}):
+        params_encoded = urllib.urlencode(params)
+        url = endpoint + '?' + params_encoded
+        eventbrite = Eventbrite(EB_ACCESS_TOKEN)
+        response = [
+            element
+            for element in eventbrite.get(
+                url
+            )[target]
+        ]
+        return response

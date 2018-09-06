35721f39d29c126f03ca45d283988730eaf97e8d
eb-cmoyano <cmoyano@eventbrite.com>
2018-09-06 10:35:35 -0300

    add complex element

--- integrations/facebook.py
+++ integrations/facebook.py
@@ -2,10 +2,82 @@ import requests
 
 from integrations.base import Integration
 from constants import FB_MESSENGER_ACCESS_TOKEN
+from integrations.exceptions import UndefinedElementType
+
+
+class FacebookSimpleElement(object):
+    def get_element(self, title, sub, image_url, btn_title, btn_url):
+        element = {
+            "title": title,
+            "subtitle": sub,
+            "image_url": image_url,
+            "buttons": [{
+                "title": btn_title,
+                "type": "web_url",
+                "url": btn_url,
+                "messenger_extensions": "false",
+                "webview_height_ratio": "full"
+            }],
+            "default_action": {
+                "type": "web_url",
+                "url": btn_url,
+                "messenger_extensions": "false",
+                "webview_height_ratio": "full"
+            }
+        }
+
+        return element
+
+
+class FacebookComplexElement(object):
+    def get_element(self, title, sub, image_url, btn_title, btn_url):
+        element = {
+            'title': title,
+            'subtitle': sub,
+            'item_url': btn_url,
+            'image_url': image_url,
+            'buttons':
+                [
+                    {
+                        "type": "payment",
+                        "title": "buy",
+                        "payload": "ticket_type_vip",
+                        "payment_summary": {
+                            "currency": "USD",
+                            "payment_type": "FIXED_AMOUNT",
+                            "is_test_payment": "true",
+                            "merchant_name": "Eventbrite",
+                            "requested_user_info": [
+                                "contact_email"
+                            ],
+                            "price_list":[
+                                {
+                                    "label": "Subtotal",
+                                    "amount": "1.00"
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        "title": "$100 Get tickets",
+                        'type': 'web_url',
+                        "url": "https://mulberry-surf.glitch.me/webview",
+                        "webview_height_ratio": "tall",
+                        "messenger_extensions": "true",
+                    }
+                ],
+        }
+        return element
 
 
 class FacebookIntegration(Integration):
     """docstring for FacebookIntegration"""
+
+    ELEMENTS_TYPE = {
+        'simple': FacebookSimpleElement,
+        'complex': FacebookComplexElement,
+    }
+
     def __init__(self, call_url='https://graph.facebook.com/v2.6/me/messages'):
         super(FacebookIntegration, self).__init__()
         self.fb_token = FB_MESSENGER_ACCESS_TOKEN
@@ -38,23 +110,9 @@ class FacebookIntegration(Integration):
         print(sender_id)
         print(json_data)
 
-    def get_element(self, title, sub, image_url, btn_title, btn_url):
-        element = {
-            "title": title,
-            "subtitle": sub,
-            "image_url": image_url,
-            "buttons": [{
-                "title": btn_title,
-                "type": "web_url",
-                "url": btn_url,
-                "messenger_extensions": "false",
-                "webview_height_ratio": "full"
-            }],
-            "default_action": {
-                "type": "web_url",
-                "url": btn_url,
-                "messenger_extensions": "false",
-                "webview_height_ratio": "full"
-            }
-        }
-        return element
+    def get_element(self, element_type, title, sub, image_url, btn_title, btn_url):
+        element = self.ELEMENTS_TYPE.get(element_type, None)
+        if element:
+            return element().get_element(title, sub, image_url, btn_title, btn_url)
+        else:
+            raise UndefinedElementType(element_type)

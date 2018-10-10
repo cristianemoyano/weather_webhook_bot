b6604398ad4325b38cc6f08dfd4aa6b5d92837e9
eb-cmoyano <cmoyano@eventbrite.com>
2018-10-10 12:31:29 -0300

    fix views

--- chatbot/views/base.py
+++ chatbot/views/base.py
@@ -5,10 +5,11 @@ class View(object):
     """HTML views"""
     def __init__(self):
         super(View, self).__init__()
+        self.view_path = '/chatbot/templates/'
         self.template_name = 'undefined'
 
     def render(self):
         raise NotImplementedError()
 
     def get_template_path(self, extension='.html'):
-        return ROOT_PATH + '/templates/' + self.template_name + extension
+        return ROOT_PATH + self.view_path + self.template_name + extension

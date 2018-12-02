d634a7145567b0ee5b02c7f284c29852f56de1cd
eb-cmoyano <cmoyano@eventbrite.com>
2018-12-02 13:58:25 -0300

    fix sandbox

--- chatbot/agents/sandbox.py
+++ chatbot/agents/sandbox.py
@@ -1,4 +1,5 @@
 from chatbot.agents.base import Agent
+from chatbot.constants import DEBUG
 from chatbot.agents.welcome import WelcomeAgent
 from chatbot.integrations.sandbox import (
     get_facebook_welcome_payload,
@@ -13,20 +14,22 @@ class SandBoxAgent(Agent):
     def __init__(self):
         super(SandBoxAgent, self).__init__()
         self.welcome_agent = WelcomeAgent()
+        self.is_debug = DEBUG
 
     def process_request(self, post):
-        print('-----------------SANDBOX------------------')
-        print(post)
-        print('-----------------SANDBOX------------------')
-        action = post.get('action')
-        if action == WELCOME_ACTION:
-            request = get_facebook_welcome_payload(
-                sender_id=post.get('sender_id', '2093633150674633'),
-                recipient_id=post.get('recipient_id', '256171418366507')
-            )
-            print(request)
-            self.welcome_agent.process_request(request)
-        elif action == SIMPLE_FB_MSG_ACTION:
-            print(SIMPLE_FB_MSG_ACTION)
-        else:
-            print(BLANK_ACTION)
+        if self.is_debug:
+            print('-----------------SANDBOX------------------')
+            print(post)
+            print('-----------------SANDBOX------------------')
+            action = post.get('action')
+            if action == WELCOME_ACTION:
+                request = get_facebook_welcome_payload(
+                    sender_id=post.get('sender_id', '2093633150674633'),
+                    recipient_id=post.get('recipient_id', '256171418366507')
+                )
+                print(request)
+                self.welcome_agent.process_request(request)
+            elif action == SIMPLE_FB_MSG_ACTION:
+                print(SIMPLE_FB_MSG_ACTION)
+            else:
+                print(BLANK_ACTION)

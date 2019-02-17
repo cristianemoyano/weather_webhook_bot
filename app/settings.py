65e6f6c25794e358ed578ce3d2b1657648544c03
eb-cmoyano <cmoyano@eventbrite.com>
2019-02-16 21:51:09 -0300

    fix static

--- app/settings.py
+++ app/settings.py
@@ -64,8 +64,8 @@ THIRD_PARTY_APPS = (
 INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

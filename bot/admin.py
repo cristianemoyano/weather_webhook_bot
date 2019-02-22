b0203e720e164f14165024b0275ca09238cadad0
eb-cmoyano <cmoyano@eventbrite.com>
2019-02-22 10:38:22 -0300

    add admin

--- bot/admin.py
+++ bot/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from rest_framework.authtoken.admin import TokenAdmin
-from .models import SocialAccount
+from .models import SocialAccount, SocialPages
 
 TokenAdmin.raw_id_fields = ('user',)
 
 admin.site.register(SocialAccount)
+admin.site.register(SocialPages)
